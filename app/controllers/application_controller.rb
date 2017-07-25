aef76b59b18e0a104e6f0464a3cc06eebc763009
LiemNguyen <liemnguyenx@gmail.com>
2017-07-25 14:22:20 +0700

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: "hello, world!"
+  end
+
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+end
